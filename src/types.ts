c2a10a9c495c79ec74691e2b37c82b34d097e8f2
Pedro Rezende <pedrorezende@users.noreply.github.com>
2025-11-24 20:37:37 -0300

    feat: updating components and creating first route (#8)
    * feat: updating transaction item props
    
    * feat: installing tanstack router and creating first route

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,17 @@ export type TokenBalance = {
   amount: bigint;
   network?: string;
 };
+
+export const TRANSACTION_STATUS = [
+  "sent",
+  "sending",
+  "waiting-receiver",
+  "received",
+  "receiving",
+  "deposited",
+  "depositing",
+  "withdraw",
+  "withdrawing",
+] as const;
+
+export type TransactionStatus = (typeof TRANSACTION_STATUS)[number];
