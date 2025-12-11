c15a872a2c8a73581d4881ae7f20d08a13722c1c
Harrison Mendonça <harrisonmendonca@gmail.com>
2025-12-11 10:37:33 +0100

    feat: add request and pay page (#93)

--- src/lib/keyUtils.ts
+++ src/lib/keyUtils.ts
@@ -1,4 +1,5 @@
 import type { UserKeyring, UserPublicKeys } from "types";
+import { type PayAddress, encodePayAddress } from "./payAddress";
 
 export const getUserPublicKeysFromKeyring = (
   keyring: UserKeyring
@@ -9,4 +10,10 @@ export const getUserPublicKeysFromKeyring = (
     discoveryPublicKey: keyring.discoveryKeyPair.publicKey,
     encryptionPublicKey: keyring.encryptionKeyPair.publicKey,
   };
+};
+
+export const getPayAddressFromKeyring = (
+  keyring?: UserKeyring
+): PayAddress | undefined => {
+  return keyring && encodePayAddress(getUserPublicKeysFromKeyring(keyring));
 };
