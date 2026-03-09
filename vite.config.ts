c9406c281b0130af907e4da8de5b8b5f052f994a
Justin R. Evans <jurevans@gmail.com>
2026-03-09 10:44:24 -0400

    fix: remove jsdom, switch test env to node & fix unit tests

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,13 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [dts(), tsconfigPaths()],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       fileName: "anomapay-sdk",
-      formats: ["es", "cjs"],
+      name: "anomapay-sdk",
+      formats: ["es", "umd"],
     },
   },
+  plugins: [dts(), tsconfigPaths()],
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,10 @@
-import { fileURLToPath } from "node:url";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { configDefaults, defineConfig } from "vitest/config";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: [fileURLToPath(new URL("./tests/setup.ts", import.meta.url))],
-    css: true,
-    exclude: [...configDefaults.exclude, "tests/e2e/**"],
+    environment: "node",
   },
 });
