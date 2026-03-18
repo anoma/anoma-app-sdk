6fbdccdcc1b3fdc4ba9a0686a4d4421ae3f3cbe7
Pedro Rezende <pedrorezende@users.noreply.github.com>
2026-03-18 12:36:51 -0300

    feat: preparing ui for public beta (#473)
    * feat: final message signature for anoma pay
    
    * feat: changing BetaWarning banner text
    
    * feat: removing request button from dashboard temporarily

--- src/signing-message.ts
+++ src/signing-message.ts
@@ -1,2 +1,3 @@
+// Changing this message will invalidate all existing keyrings, so be careful when modifying it.
 export const getSignMessage = (address: string) =>
-  `I authorize AnomaPay to derive my account from address ${address}.\nDo NOT sign this message if the request url is not https://beta.anomapay.app/`;
+  `I authorize AnomaPay to derive my account from address ${address}.\nDo NOT sign this message if the request url is not https://anomapay.app`;
