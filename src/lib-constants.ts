722506c9f0fb09cdab20c1d3705b9b8346e08211
Pedro Rezende <xpedrorezende@gmail.com>
2026-04-01 04:02:59 -0300

    refactor: rollback app constants

--- src/lib-constants.ts
+++ src/lib-constants.ts
@@ -1,4 +1,3 @@
-import type { TokenId } from "types";
 import type { Address, Chain } from "viem";
 import { base, bsc, mainnet, sepolia } from "viem/chains";
 
@@ -15,44 +14,14 @@ export const TRANSFER_LOGIC_VERIFYING_KEY =
 // Authorization signature domain
 export const AUTH_SIGNATURE_DOMAIN = "TokenTransferAuthorization";
 
-// Constants related to vault creation
-// Defines the current version of the Vault. Should be incremented on every
-// update of the vault structure, so users can migrate from old versions to the
-// current one without the need of recreating the vault
-export const VAULT_VERSION = 1;
-
 // Separate the encryption of the vault into separate domains
 export const KEYRING_SALT = "anoma-pay:keyring-seed";
-export const PASSKEY_DOMAIN = "anoma-pay:passkeys";
-export const RETURNING_USER_STORAGE_KEY = "anoma-pay:returning_user";
-
-export const WEBSITE_URL = "https://anoma.money";
-export const WEBSITE_DOMAIN = "anoma.money";
-export const TERMS_URL = "https://anomapay.app/terms-and-conditions";
-
-// General
 
 // Estimate time to calculate a proof:
 export const averageTimePerProofInSeconds = 20;
 
-// Polling interval to check for user balances:
-export const balanceRefetchIntervalInMs = 10_000;
-
-export const defaultTokenId: TokenId = "bsc:usdc";
-
-/**
- * The folloiwng constants determine the normal load threshhold,
- * beyond which the queue could be said to be under heavy load
- */
-export const provingGPUs = 11; // TODO: Use value from endpoint when availalbe
 export const proofPerTx = 3;
 export const estimatedTxTimeInSeconds = 120;
-
-// Polling interval to check for Bento queue stats:
-export const statsQueueRefetchIntervalInMs = 20_000;
-
-// How many times some mutations query should retry before throwing an error:
-export const retryMutationsCount = 3;
 
 // Blocks deposit if total amount is bigger than maxBalanceInUsd
 export const maxBalanceInUsd = Number(
