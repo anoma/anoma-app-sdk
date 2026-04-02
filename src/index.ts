334c82ca561955849309375e00985610ba746e4d
Pedro Rezende <xpedrorezende@gmail.com>
2026-04-02 00:56:55 -0300

    fix: resolve ambiguity in NullifierKeyPair name

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-export * from "api";
-export * from "domain";
-export * from "lib";
-export * from "types";
-export * from "wasm";
+export * from "./api";
+export * from "./domain";
+export * from "./lib";
+export * from "./types";
+export * from "./wasm";
+
+// Resolve NullifierKeyPair ambiguity between domain and wasm
+export { NullifierKeyPair } from "./domain";
+export { NullifierKeyPair as WasmNullifierKeyPair } from "./wasm";
