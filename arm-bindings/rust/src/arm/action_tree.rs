6291aa55003dbc41c65b13cf14e511a713ba9bc6
Justin R. Evans <jurevans@gmail.com>
2026-09-22 10:38:53 -0400

    chore: bump to latest arm 2.x.x crates, fix errors

--- arm-bindings/rust/src/arm/action_tree.rs
+++ arm-bindings/rust/src/arm/action_tree.rs
@@ -1,22 +1,22 @@
 use crate::arm::digest::Digest;
 use crate::error::BindingsError;
-use arm::action_tree::MerkleTree as MT;
+use arm::action_tree::ActionTree as AT;
 use std::sync::Arc;
 
 #[derive(Debug, uniffi::Object)]
-pub struct MerkleTree(pub(crate) MT);
+pub struct ActionTree(pub(crate) AT);
 
-impl MerkleTree {
-    pub fn instance(&self) -> &MT {
+impl ActionTree {
+    pub fn instance(&self) -> &AT {
         &self.0
     }
 }
 
 #[uniffi::export]
-impl MerkleTree {
+impl ActionTree {
     #[uniffi::constructor]
     pub fn new(leaves: Vec<Arc<Digest>>) -> Self {
-        MerkleTree(MT::new(leaves.iter().map(|d| d.0).collect()))
+        ActionTree(AT::new(leaves.iter().map(|d| d.0).collect()))
     }
 
     pub fn root(&self) -> Result<Digest, BindingsError> {
